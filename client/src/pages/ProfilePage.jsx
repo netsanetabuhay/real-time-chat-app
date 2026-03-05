80868bc58a566ca05f7357f092ff24f8f73c3661
Netsanet Abuhay <netsanetabuhay@gmail.com>
2026-03-05 14:28:13 +0300

    AFTER INITIAL THE PROFILE PAGE SETUP BUT NOT FULLY TESTED

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>profile pages</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flext items-center justify-center'>
+      <div className='w-6/5 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form action="" className='flex flext-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg '>profile details</h3>
+
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
