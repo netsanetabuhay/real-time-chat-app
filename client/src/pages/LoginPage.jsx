3ea7a1bf5ecca8b5c06abd6c2123cc681855b060
Netsanet Abuhay <netsanetabuhay@gmail.com>
2026-03-05 12:59:22 +0300

    after login page fixed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,16 +8,21 @@ const LoginPage = () => {
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
 const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+const onsubmitHandler=(event)=>{event.preventDefault();
+  if(currentState==='sign up'&& !isDataSubmitted){setIsDataSubmitted(true)
+    return;
+  }
+}
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
       {/* ............left............. */}
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
       {/*.....right...... */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+      <form onSubmit={onsubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
-          {currentState}
-          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />
+          {currentState}{isDataSubmitted &&
+          <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />}
         </h2>
         
         {currentState === "sign up" && !isDataSubmitted && (
