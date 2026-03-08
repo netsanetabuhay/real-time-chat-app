a75f31f755bbd5739a0d8e499bac4efbeccd7db2
Netsanet Abuhay <netsanetabuhay@gmail.com>
2026-03-08 22:03:12 +0300

    the initial integration backend with frontend just the login initialy make the each route to be protected

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,154 @@
+import React, { createContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { io } from 'socket.io-client';
+import axios from 'axios';
+
+
+
+
+const backendUrl= import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL=backendUrl;
+
+
+
+export const AuthContext = createContext(); // eslint-disable-line
+
+const AuthProvider = ({children}) => {
+
+const [token, setToken] = useState(localStorage.getItem("token"));
+const [authUser , setAuthUser] = useState(null)
+const [onlineUser ,setOnlineUser] = useState([]);
+const [socket , setSocket] = useState(null);
+
+//check if user is authenticated and if so , set the user date and connect the socket
+
+const checkAuth = async () => {
+    try {
+   const {data} =  await axios.get("api/auth/check");
+
+         if(data.success){
+            setAuthUser(data.user);
+            connectSocket(data.user);
+         }
+        
+
+    } catch (error) {
+        toast.error(error.message);
+        
+    }
+
+}
+
+
+//login function to handle user authentication and socket connection
+
+const login = async (state, credentials)=>{
+
+    try {
+const {data}= await axios.post(`/api/auth/${state}`, credentials);
+if(data.success){
+    setAuthUser(data.userData);
+    connectSocket(data.userData);
+    axios.defaults.headers.common["token"]=data.token;
+    setToken(data.token);
+    localStorage.setItem("token", data.token);
+    toast.success(data.message);
+
+}
+else{
+    toast.error(data.message);
+}
+        
+    } catch (error) {
+        toast.error(error.message);
+        
+    }
+
+}
+
+//logout function to handle user logout and socket disconnection
+
+const logout = async () =>{
+    
+        localStorage.removeItem("token");
+        setToken(null);
+        setAuthUser(null);
+        setOnlineUser([]);
+        axios.defaults.headers.common["token"]=null; 
+        toast.success("Logged out successfully");
+         if (socket) { 
+        
+        socket.disconnect();}
+}
+//update profile function to handle user profile updates and socket reconnection
+
+const updateProfile = async(body)=>{
+    try {
+        const {data} = await axios.put("/api/auth/update-profile", body);
+        if(data.success){
+            setAuthUser(data.user);
+            toast.success('Profile updated successfully ');
+        }
+     
+    } catch (error) {
+        toast.error(error.message);
+        
+    }
+
+}
+
+//connet socket function to handle the socket connection and online user updates
+
+const connectSocket= (userData)=>{
+    if(!userData|| socket?.connected) return; 
+    const newSocket = io(backendUrl, {
+        query:{
+            userId: userData._id,
+
+        }
+    });
+    newSocket.connect();
+    setSocket(newSocket);
+    newSocket.on("getOnlineUsers", (userIds)=>{
+        setOnlineUser(userIds);
+    })
+    
+    }
+
+useEffect(()=>{
+    if(token)
+{
+    axios.defaults.headers.common["token"] = token;
+    checkAuth();
+
+
+
+}
+
+}, []);
+
+
+
+
+    const value= (
+        axios,
+        authUser,
+        onlineUser,
+        socket,
+        login,
+        logout,
+        updateProfile
+
+);
+
+
+    return (<AuthContext.Provider value={value}>
+        {children}
+        </AuthContext.Provider>
+        
+        
+        );
+
+} 
+  
+export default AuthProvider;
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 // Import controller functions for handling message-related operations
-import {getUsersForSidebar, getMessages, markMessageAsSeen} from '../controller/messageController.js';
+import {getUsersForSidebar, getMessages, markMessageAsSeen, sendMessage} from '../controller/messageController.js';
 
 // Import authentication middleware to protect routes
-import protectRoutes from '../middleware/auth.js';
+import {protectRoutes} from '../middleware/auth.js';
 
 // Create a new router instance
 const router = express.Router();
@@ -12,5 +12,6 @@ const router = express.Router();
 router.get('/users', protectRoutes, getUsersForSidebar);
 router.get('/:id/', protectRoutes, getMessages);
 router.post('/mark/:id', protectRoutes, markMessageAsSeen);
+router.post("/send/:id" , protectRoutes, sendMessage);
 
 export default router;
