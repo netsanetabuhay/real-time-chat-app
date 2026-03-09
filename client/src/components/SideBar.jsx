6c0ed38ff0ca6659b60e019c551933cca6fc5adc
Netsanet Abuhay <netsanetabuhay@gmail.com>
2026-03-09 15:17:29 +0300

    after the chat one to one is done by integrating with backend

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import assets, {userDummyData} from "../assets/assets";
+import assets from "../assets/assets";
+import { AuthContext } from '../../context/AuthContext';
 
-const SideBar = ({ selectedUser, setSelectedUser }) => {
+const SideBar = () => {
+  const {getUser, users, selectedUeser, setSelectedUserl unseenMessages, setUnseenMessages
+  }   =  useContext(AuthContext);
+
+
+  const {logout, onlineUsers} = useContext(AuthContext);
+
+  cosnt [input, setInput] = useState(false);
+
     const navigate = useNavigate();
+
+    const filterdUsers= input ? users.fileter((user)=>user.fullName.tolowerCase().includes(input.tolowerCase())):users;
+
+    useEffect(()=>{
+      getUsers();
+
+
+    },[onlineUsers])
 
     return (
        
@@ -17,14 +34,14 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
                     <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
                         <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
                         <hr className="my-2 border-t border-gray-500"/>
-                        <p className='cursor-pointer text-sm'>Logout</p>
+                        <p onClick={()=>logout()}       className='cursor-pointer text-sm'>Logout</p>
                     </div>
                 </div>
             </div>
 
             <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mb-5'>
                 <img src={assets.search_icon} alt="Search" className='w-3'/>
-                <input 
+                <input onChange={(e)=setInput(e.target.value)}
                     type="text" 
                     className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' 
                     placeholder='Search User...'
@@ -34,7 +51,7 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
           
         </div>
           <div className='flex flex-col'>
-  {userDummyData.map((user, index) => (
+  {filterdUsers.map((user, index) => (
     <div
       onClick={() => setSelectedUser(user)}
       key={index}
@@ -47,15 +64,15 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
       />
       <div className='flex flex-col leading-5'>
         <p>{user.fullName}</p>
-        {index < 3 ? (
+        {onlineUsers.includes(users._id) ? (
           <span className='text-green-400 text-xs'>Online</span>
         ) : (
           <span className='text-neutral-400 text-xs'>Offline</span>
         )}
       </div>
-      {index > 2 && (
+      {unseenMessages.[user._id]>0 && (
         <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>
-          {index}
+          {unseenMessages.[user._id]}
         </p>
       )}
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from '../context/AuthContext.jsx'
+import  chatProvider  from '../context/chatContext.jsx';  
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
-        <App />
+    <chatProvider>
+      <App />
+      </chatProvider>
   </AuthProvider>
   </BrowserRouter>,
 )
